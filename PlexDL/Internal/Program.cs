8f5223aac97f9322324c64ef54df048e65759587
Baeleigh Harris <baeleighharris@outlook.com>
2020-05-11 19:26:15 +1000

    Added 'Open With' functionality

--- PlexDL/Internal/Program.cs
+++ PlexDL/Internal/Program.cs
@@ -1,6 +1,9 @@
-﻿using PlexDL.UI;
+﻿using PlexDL.Common.API;
+using PlexDL.Common.Structures.Plex;
+using PlexDL.UI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Windows.Forms;
@@ -26,10 +29,48 @@ namespace PlexDL.Internal
             //allow heaps of concurrent connections - this might make a difference for performance?
             //disable if you have any problems by commenting this line
             ServicePointManager.DefaultConnectionLimit = 128;
-            if (arr.Contains("-t"))
-                Application.Run(new Translator());
+
+            if (arr.Count > 0)
+            {
+                string firstArg = arr[0];
+                //Windows will pass "Open With" files as the first argument; checking if the first argument
+                //exists as a file will validate whether this has occurred.
+                if (File.Exists(firstArg))
+                {
+                    //Windows has passed a file; we need to check if it's a '.pmxml' file which we can load
+                    //into the Metadata window.
+                    string ext = Path.GetExtension(firstArg);
+                    string expectedFormat = ".pmxml";
+                    if (string.Equals(ext, expectedFormat))
+                    {
+                        var metadata = ImportExport.MetadataFromFile(firstArg);
+                        RunMetadataWindow(metadata);
+                    }
+                    else
+                        MessageBox.Show("PlexDL doesn't recognise this file-type: '" + ext + "'", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (arr.Contains("-testing"))
+                        Application.Run(new TestForm());
+                    else if (arr.Contains("-t"))
+                        Application.Run(new Translator());
+                }
+            }
             else
                 Application.Run(new Home());
+        }
+
+        private static void RunMetadataWindow(PlexObject metadata)
+        {
+            var form = new Metadata();
+            if (metadata != null)
+            {
+                form.StreamingContent = metadata;
+                Application.Run(form);
+            }
+            else
+                MessageBox.Show("Invalid PlexMovie Metadata File; the decoded data was null.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
         private static void VisualStyles(List<string> args)
