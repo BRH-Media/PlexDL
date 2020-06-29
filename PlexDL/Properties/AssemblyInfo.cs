81b0f8ef852f5d80c577a94adc4d998272270973
Baeleigh Harris <baeleighharris@outlook.com>
2020-06-29 22:03:56 +1000

    Update AssemblyInfo.cs

--- PlexDL/Properties/AssemblyInfo.cs
+++ PlexDL/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.5.3.*")]
-[assembly: AssemblyFileVersion("1.5.3.*")]
+[assembly: AssemblyVersion("1.5.5.*")]
+[assembly: AssemblyFileVersion("1.5.5.*")]
 [assembly: NeutralResourcesLanguage("en-AU")]
