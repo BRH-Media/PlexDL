255e7f3c58c5c2dc7d45a42693c55be30515b5b1
Baeleigh Harris <baeleighharris@outlook.com>
2022-04-11 02:34:50 +1000

    Version bump to v1.7.3

--- PlexDL/Properties/AssemblyInfo.cs
+++ PlexDL/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.2.*")]
-[assembly: AssemblyFileVersion("1.7.2.*")]
+[assembly: AssemblyVersion("1.7.3.*")]
+[assembly: AssemblyFileVersion("1.7.3.*")]
 [assembly: NeutralResourcesLanguage("en-AU")]
