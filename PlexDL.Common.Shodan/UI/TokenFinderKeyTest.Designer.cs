f0cea2cc0c40d5cc9d9a432675b9dc5ba6f60457
Baeleigh Harris <baeleighharris@outlook.com>
2022-04-15 23:29:55 +1000

    Token finder updates

---
+++ PlexDL.Common.Shodan/UI/TokenFinderKeyTest.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PlexDL.Common.Shodan.UI
+{
+    partial class TokenFinderKeyTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PlexDL.Common.Components.Styling.BoolColour boolColour2 = new PlexDL.Common.Components.Styling.BoolColour();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TokenFinderKeyTest));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.gbResults = new System.Windows.Forms.GroupBox();
+            this.dgvResults = new PlexDL.Common.Components.Controls.FlatDataGridView();
+            this.tlpMain.SuspendLayout();
+            this.gbResults.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 2;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpMain.Controls.Add(this.btnCancel, 0, 1);
+            this.tlpMain.Controls.Add(this.btnOK, 1, 1);
+            this.tlpMain.Controls.Add(this.gbResults, 0, 0);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 2;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpMain.Size = new System.Drawing.Size(334, 218);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCancel.Location = new System.Drawing.Point(3, 192);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(161, 23);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOK.Location = new System.Drawing.Point(170, 192);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(161, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // gbResults
+            // 
+            this.tlpMain.SetColumnSpan(this.gbResults, 2);
+            this.gbResults.Controls.Add(this.dgvResults);
+            this.gbResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbResults.Location = new System.Drawing.Point(3, 3);
+            this.gbResults.Name = "gbResults";
+            this.gbResults.Size = new System.Drawing.Size(328, 183);
+            this.gbResults.TabIndex = 2;
+            this.gbResults.TabStop = false;
+            this.gbResults.Text = "Results";
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.AllowUserToAddRows = false;
+            this.dgvResults.AllowUserToDeleteRows = false;
+            this.dgvResults.AllowUserToOrderColumns = true;
+            this.dgvResults.AllowUserToResizeRows = false;
+            this.dgvResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvResults.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            boolColour2.BoolColouringEnabled = false;
+            boolColour2.ColouringMode = PlexDL.Common.Components.Styling.BoolColourMode.BackColour;
+            boolColour2.FalseColour = System.Drawing.Color.DarkRed;
+            boolColour2.RelevantColumns = ((System.Collections.Generic.List<string>)(resources.GetObject("boolColour2.RelevantColumns")));
+            boolColour2.TrueColour = System.Drawing.Color.DarkGreen;
+            this.dgvResults.BoolColouringScheme = boolColour2;
+            this.dgvResults.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvResults.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dgvResults.CellContentClickMessage = false;
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvResults.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvResults.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.dgvResults.IsContentTable = false;
+            this.dgvResults.Location = new System.Drawing.Point(3, 16);
+            this.dgvResults.MultiSelect = false;
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.RowHeadersVisible = false;
+            this.dgvResults.RowsEmptyText = "No Results";
+            this.dgvResults.RowsEmptyTextForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(134)))), ((int)(((byte)(134)))));
+            this.dgvResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvResults.Size = new System.Drawing.Size(322, 164);
+            this.dgvResults.TabIndex = 0;
+            // 
+            // TokenFinderKeyTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(334, 218);
+            this.Controls.Add(this.tlpMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TokenFinderKeyTest";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "API Key Test";
+            this.Load += new System.EventHandler(this.TokenFinderKeyTest_Load);
+            this.tlpMain.ResumeLayout(false);
+            this.gbResults.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.GroupBox gbResults;
+        private Components.Controls.FlatDataGridView dgvResults;
+    }
+}
---
+++ PlexDL.Common.Shodan/UI/TokenFinderKeyTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using UIHelpers;
+
+namespace PlexDL.Common.Shodan.UI
+{
+    public partial class TokenFinderKeyTest : Form
+    {
+        public string Token { get; set; } = @"";
+
+        public TokenFinderKeyTest()
+        {
+            InitializeComponent();
+        }
+
+        private void TokenFinderKeyTest_Load(object sender, EventArgs e)
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                //alert the user
+                UIMessages.Error($"API Key test loading error:\n\n{ex}");
+
+                //exit
+                Close();
+            }
+        }
+    }
+}
